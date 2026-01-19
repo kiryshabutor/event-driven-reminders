c8a4b422335ede84357ba2cdc6e82dce3e6d34aa
kiribu <kiryshabutor@gmail.com>
2026-01-19 20:00:31 +0300

    change user struct and add RefreshToken

--- models/user.go
+++ models/user.go
@@ -3,11 +3,20 @@ package models
 import "time"
 
 type User struct {
-	ID           int       `json:"id"`
-	Username     string    `json:"username"`
-	PasswordHash string    `json:"-"`
-	CreatedAt    time.Time `json:"created_at"`
+	ID           int       `db:"id" json:"id"`
+	Username     string    `db:"username" json:"username"`
+	PasswordHash string    `db:"password_hash" json:"-"`
+	CreatedAt    time.Time `db:"created_at" json:"created_at"`
 }
+
+type RefreshToken struct {
+	ID        int       `db:"id" json:"id"`
+	Token     string    `db:"token" json:"token"`
+	UserID    int       `db:"user_id" json:"user_id"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	ExpiresAt time.Time `db:"expires_at" json:"expires_at"`
+}
+
 type Credentials struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
